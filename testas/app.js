a2ad07d275279dc084b8d7aa48162e4cdb494458
valdasdamidavicius <valdukasirko@gmail.com>
2024-03-21 10:34:58 +0200

    testas

--- testas/app.js
+++ testas/app.js
@@ -4,3 +4,25 @@ function rand(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// Programiškai ridenkite du žaidimo kauliukus- sugeneruokite du atsitiktinius skaičius nuo 1 iki 6 
+// Jeigu kauliukų suma didesnė nei 8 jūs laimėjote, priešingu atveju pralošėte. Išveskite atsakymą,
+//  kuriame būtų abiejų kauliukų reikšmės ir išvada laimėjote ar pralošėte.
+
+
+
+let a = rand(1, 6) * 2;
+
+if (a > 8) {
+    a = 'Winner';
+} else {
+    a = 'Losser';
+}
+
+console.log(a);
+
+
+
+
+
+
+
