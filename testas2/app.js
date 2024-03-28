7b90670e8d6fc77b22a5e2d2aac05d4eab3bd96e
valdasdamidavicius <valdukasirko@gmail.com>
2024-03-28 10:30:09 +0200

    testas2

--- testas2/app.js
+++ testas2/app.js
@@ -4,3 +4,11 @@ function rand(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// Vyksta automobilių žiedinės lenktynės. Automobiliui Nr. 55 liko nuvažiuoti 10 ratų. Suprogramuokite for ciklą, kuris imituotų 10 ratų automobilio važiavimą ir kiekviename cikle pateiktų (atspausdintų) kiek ratų dar liko automobiliui nuvažiuoti. Paskutinis skaičius turėtų būti 1. Visas rezultatas turėtų būti toks:  10 9 8 7 6 5 4 3 2 1 (skaičiai gali būti atspausdinti stulpeliu).
+
+
+
+
+for (let i = 1; i < 11; i++) {
+    console.log(i);
+}
