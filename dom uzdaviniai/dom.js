f291378d4e50c3db519fadd2673386f8de285551
valdasdamidavicius <valdukasirko@gmail.com>
2024-04-15 08:42:38 +0300

    dom uzdaviniai

--- dom uzdaviniai/dom.js
+++ dom uzdaviniai/dom.js
@@ -11,6 +11,7 @@ window.addEventListener('load', _ => {
     // naudojant css selektorius;
     // naudojant style savybę;
 
+    // Ieškojimas DOM’e
 
     // Konsolėje atspausdinti H1 tekstą;
 
@@ -50,8 +51,33 @@ window.addEventListener('load', _ => {
 
 
 
+    // DOM’o modifikavimas
+
+    // “Zoo sodas” pakeiskite į “Zoo parkas”;
+    // “***” pakeiskite į “*****”;
+    // Pridėkite naują žvėrį “Leopardas” į plėšrūnų sekcijos pabaigą;
+    // Pridėkite naują žvėrį “Jūrų sliekas” į gyvačių sekcijos pradžią;
+    // Pridėkite “Atsinaujinom” span tage prie žoliaėdžių;
+    // Ištrinkite “Atsinaujinom” span tage iš gyvačių;
+    // Ištrinkite “Laukinę kiaulę”;
+    // Apačioje h2 tago “Plėšrūnai” pridėkite h3 tagą “Neglostyti!”;
+    // Pridėkite naują kainą “Studentas 3eur” po tago “Vaikas: 2eur”;
+    // Išrūšiuokita “Plėšrūnus pagal abėcėlę”;
 
 
+
+    // DOM’o stilius.
+
+    // Nuspalvinkite H1 tagą raudonai;
+    // Kontaktus nuspalvinkite pilkai, o adresą span tage palikite juodą;
+    // Žvėrių tipų H2 tagus nuspalvinkite mėlynai;
+    // “PATINKA” mygtuką apibrėžkite rėmeliu;
+    // Nuspalvinkite visus naujus žvėris geltona spalva su pilku fonu;
+    // Naudodami stiliaus savybes paslėpkite “Zuikis” li tagą;
+    // Panaikinkite sąrašuose “rutuliukus” prieš žvėries pavadinimą;
+    // Padidinkite tarpus tarp žvėrių;
+    // Tagus span “Atsinaujinom” nuspalvinkite raudonai;
+    // Tagus span “Atsinaujinom” padarykite mirksinčius (pasinaudokite setInterval);
 
 
 
