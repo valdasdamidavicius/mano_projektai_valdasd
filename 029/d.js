6dd2cb92cc4b7d22014fc71b5c5f25731cf80d7e
valdasdamidavicius <valdukasirko@gmail.com>
2024-04-18 15:44:19 +0300

    029

---
+++ 029/d.js
@@ -0,0 +1,18 @@
+console.log('Welcome');
+
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+window.addEventListener('load', _ => {
+
+// pasirinkus spalvą ir perkrovus puslapį spalva turi pasilikti pasirinkta.
+
+
+
+
+
+
+});
