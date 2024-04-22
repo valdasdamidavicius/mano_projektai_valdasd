228bfe60e4a4c54fb4e9a587431180d92b2322b9
valdasdamidavicius <valdukasirko@gmail.com>
2024-04-22 15:49:11 +0300

    030

---
+++ 030/d.js
@@ -0,0 +1,18 @@
+console.log('Welcome');
+
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+window.addEventListener('load', _ => {
+
+
+
+
+
+
+
+
+});
