2916baa9a232049c3854a5af89ddad82cb848453
valdasdamidavicius <valdukasirko@gmail.com>
2024-05-06 15:41:26 +0300

    033

---
+++ 033/src/app.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', _ => {
+
+    const readButton = document.querySelector('#r');
+    const writeButton = document.querySelector('#w');
+
+    let counter = 0;
+
+    writeButton.addEventListener('click', _ => {
+
+    });
+});
