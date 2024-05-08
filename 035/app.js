8ee32a2ad149d54cc9cab5570440aed4243a33fb
valdasdamidavicius <valdukasirko@gmail.com>
2024-05-08 15:11:54 +0300

    035

---
+++ 035/app.js
@@ -0,0 +1,25 @@
+
+
+const obj = {
+    animal: 'Fox',
+    tail: '30'
+}
+
+
+
+class Fox {
+
+    constructor() {
+        this.tail = '30'
+    }
+
+}
+
+
+
+const obj2 = new Fox();
+
+
+
+
+console.log(obj, obj2);
