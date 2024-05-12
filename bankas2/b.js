d815baa02acbfdb48f9263e5395dac12bb297ffa
valdasdamidavicius <valdukasirko@gmail.com>
2024-05-12 18:15:50 +0300

    bankas2

--- bankas2/b.js
+++ bankas2/b.js
@@ -18,6 +18,9 @@ const html = `
 
 window.addEventListener('load', _ => {
 
+    const LAST_ID_LS = 'clientsLastSavedId';
+    const CLIENTS_LS = 'clientsList'
+    let destroyId = 0;
 
     const listHtml = document.querySelector('.--list');
     const closeButtons = document.querySelectorAll('.--close');
@@ -25,6 +28,18 @@ window.addEventListener('load', _ => {
 
     const createModal = document.querySelector('.modal--create');
     const storeButton = createModal.querySelector('.--submit');
+    const deleteModal = document.querySelector('.modal--delete');
+    const destroyButton = deleteModal.querySelector('.--submit');
+
+    const getId = _ => {
+        const id = localStorage.getItem(LAST_ID_LS);
+        if (null === id) {
+            localStorage.setItem(LAST_ID_LS, 1);
+            return 1;
+        }
+        localStorage.setItem(LAST_ID_LS, parseInt(id) + 1);
+        return parseInt(id) + 1;
+    }
 
     const showModal = modal => modal.style.display = 'flex';
 
@@ -35,6 +50,82 @@ window.addEventListener('load', _ => {
         modal.style.display = 'none';
     }
 
+    const getDataFromForm = form => {
+        const data = {};
+        form.querySelectorAll('[name]').forEach(i => {
+            data[i.getAttribute('name')] = i.value;
+        });
+        return data;
+    }
+
+    const store = _ => {
+        const data = getDataFromForm(createModal);  // CRUD
+        storeData(data);  // LS
+        hideModal(createModal);  // DOM
+        showList();  // DOM
+    }
+
+    const destroy = _ => {
+        destroyData(destroyId);  // LS
+        hideModal(deleteModal);  // DOM
+        showList();  //DOM
+    }
+
+    const registerDelete = _ => {
+        document.querySelectorAll('.--delete').forEach(b => {
+            b.addEventListener('click', _ => {
+                showModal(deleteModal);
+                prepareDeleteModal(parseInt(b.value));
+                destroyId = parseInt(b.value);
+            });
+        });
+    }
+
+    const destroyData = id => {
+        const data = read();
+        const deleteData = data.filter(d => d.id !== id);
+        write(deleteData);
+    }
+
+    const write = data => {
+        localStorage.setItem(CLIENTS_LS, JSON.stringify(data));
+    }
+    
+    const read = _ => {
+        const data = localStorage.getItem(CLIENTS_LS);
+        if (null === data) {
+            return [];
+        }
+        return JSON.parse(data);
+    }
+
+    const storeData = data => {
+        const storeData = read();
+        data.id = getId();
+        storeData.push(data);
+        write(storeData);
+    }
+
+    const showList = _ => {
+        let clientsHtml = '';
+        read().forEach(c => {
+            let temp = html;
+            temp = temp.replaceAll('{{id}}', c.id);
+            temp = temp.replaceAll('{{client}}', c.clientName);
+            temp = temp.replaceAll('{{client2}}', c.clientSurname);
+            temp = temp.replaceAll('{{money}}', '0');
+            clientsHtml += temp;
+        });
+        listHtml.innerHTML = clientsHtml;
+        registerDelete();
+        // registerEdit();
+    }
+
+    const prepareDeleteModal = id => {
+        const title = read().find(c => c.id == id).clientSurname;
+        deleteModal.querySelector('.clientName--clientSurname').innerText = title;
+    }
+
     closeButtons.forEach(b => {
         b.addEventListener('click', _ => {
             hideModal(b.closest('.--modal'));
@@ -42,6 +133,10 @@ window.addEventListener('load', _ => {
     });
 
     createButton.addEventListener('click', _ => showModal(createModal));
+
+    storeButton.addEventListener('click', _ => store());
+
+    destroyButton.addEventListener('click', _ => destroy());
 
     setTimeout(_ => showList(), 2000);
 
