f22be742c483db29b8529bd14382577d86841db3
valdasdamidavicius <valdukasirko@gmail.com>
2024-05-25 19:35:34 +0300

    colors

--- colors/app.js
+++ colors/app.js
@@ -13,46 +13,104 @@ app.get('/', (req, res) => {
 
     let html = fs.readFileSync('./data/index.html', 'utf-8');
     const nav = fs.readFileSync('./data/nav.html', 'utf-8');
+    let data = fs.readFileSync('./data/colors.json', 'utf-8');
+    data = JSON.parse(data);
+    const listItem = fs.readFileSync('./data/listItem.html', 'utf-8');
 
-    html = html.replace('{{NAV}}', nav);
+    let listItems = '';
+
+    data.forEach(li => {
+        let liHtml = listItem;
+        liHtml = liHtml.replaceAll('{{ID}}', li.id).replace('{{SHAPE}}', li.shape).replace('COLOR', li.color);
+        listItems += liHtml;
+    });
+
+    html = html.replace('{{NAV}}', nav).replace('{{LI}}', listItems);
 
     res.send(html);
 
 });
 
 app.get('/create', (req, res) => {
-
     let html = fs.readFileSync('./data/create.html', 'utf-8');
     const nav = fs.readFileSync('./data/nav.html', 'utf-8');
-
     html = html.replace('{{NAV}}', nav);
-
     res.send(html);
-
 });
 
 app.post('/store', (req, res) => {
-
     const color = req.body.color;
     const shape = parseInt(req.body.shape);
     const id = uuidv4();
-
     let data = fs.readFileSync('./data/colors.json', 'utf-8');
-
     data = JSON.parse(data);
-
     data.push({ id, color, shape });
-
     data = JSON.stringify(data);
-
     fs.writeFileSync('./data/colors.json', data);
-
     res.redirect(302, 'http://colors.test');
 });
 
+app.get('/delete/:id', (req, res) => {
 
+    let data = fs.readFileSync('./data/colors.json', 'utf-8');
+    data = JSON.parse(data);
+    const color = data.find(c => c.id === req.params.id);
 
+    if (!color) {
+        let html = fs.readFileSync('./data/404.html', 'utf-8');
+        res.status(404).send(html);
+    } else {
+        let html = fs.readFileSync('./data/delete.html', 'utf-8');
+    const nav = fs.readFileSync('./data/nav.html', 'utf-8');
+    html = html.replace('{{NAV}}', nav).replace('{{ID}}', color.id).replace('{{SHAPE}}', color.shape).replace('COLOR', color.color);
+    res.send(html);
+    }
+});
 
+app.post('/destroy/:id', (req, res) => {
+    
+    let data = fs.readFileSync('./data/colors.json', 'utf-8');
+    data = JSON.parse(data);
+    data = data.filter(c => c.id !== req.params.id);
+    data = JSON.stringify(data);
+    fs.writeFileSync('./data/colors.json', data);
+    res.redirect(302, 'http://colors.test');
+});
+
+app.get('/edit/:id', (req, res) => {
+
+    let data = fs.readFileSync('./data/colors.json', 'utf-8');
+    data = JSON.parse(data);
+    const color = data.find(c => c.id === req.params.id);
+    if (!color) {
+        let html = fs.readFileSync('./data/404.html', 'utf-8');
+        res.status(404).send(html);
+    } else {
+        let html = fs.readFileSync('./data/edit.html', 'utf-8');
+        const nav = fs.readFileSync('./data/nav.html', 'utf-8');
+        html = html.replace('{{NAV}}', nav).replace('{{ID}}', color.id).replace('{{SHAPE}}', color.shape).replaceAll('COLOR', color.color);
+        [1, 2, 3].forEach(v => {
+            if (v = color.shape) {
+                html = html.replace(`{{VAL${v}}}`, 'checked');
+            } else {
+                html = html.replace(`{{VAL${v}}}`, '');
+            }
+        });
+        res.send(html);
+    }
+});
+
+app.post('/update/:id', (req, res) => {
+
+    const color = req.body.color;
+    const shape = parseInt(req.body.shape);
+    let data = fs.readFileSync('./data/colors.json', 'utf-8');
+    data = JSON.parse(data);
+    data = data.map(c => c.id === req.params.id ? {...c, color, shape} : c);
+    data = JSON.stringify(data);
+    fs.writeFileSync('./data/colors.json', data);
+    res.redirect(302, 'http://colors.test');
+});
 
 
 app.listen(port, _ => {
