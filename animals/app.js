cf75bd8f77d7a0857d6278c49cd940b630f1c4b6
valdasdamidavicius <valdukasirko@gmail.com>
2024-05-28 15:44:42 +0300

    animals

--- animals/app.js
+++ animals/app.js
@@ -1,32 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const { v4: uuidv4 } = require('uuid');
+// const { v4: uuidv4 } = require('uuid');
 const app = express();
+const fs = require('node:fs');
 const port = 80;
 
 app.use(cookieParser());
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
-const fs = require('node:fs');
+
 
 const animalZoo = [
-    {name: 'Lion', species: 'Panthera leo', age: 10},
-    {name: 'Tiger', species: 'Panthera tigris', age: 12},
-    {name: 'Elephant', species: 'Loxodonta africana', age: 15},
-    {name: 'Giraffe', species: 'Giraffa camelopardalis', age: 20},
-    {name: 'Zebra', species: 'Equus zebra', age: 8},
-    {name: 'Hippopotamus', species: 'Hippopotamus amphibius', age: 25},
-    {name: 'Crocodile', species: 'Crocodylus niloticus', age: 30},
-    {name: 'Flamingo', species: 'Phoenicopterus', age: 5},
-    {name: 'Penguin', species: 'Aptenodytes forsteri', age: 3},
-    {name: 'Kangaroo', species: 'Macropus', age: 7},
-    {name: 'Koala', species: 'Phascolarctos cinereus', age: 4},
-    {name: 'Kookaburra', species: 'Dacelo novaeguineae', age: 2},
-    {name: 'Platypus', species: 'Ornithorhynchus anatinus', age: 6},
-    {name: 'Wallaby', species: 'Macropus', age: 5},
-    {name: 'Dingo', species: 'Canis lupus dingo', age: 8}
+    {id: 1, name: 'Lion', species: 'Panthera leo', age: 10},
+    {id: 2, name: 'Tiger', species: 'Panthera tigris', age: 12},
+    {id: 3, name: 'Elephant', species: 'Loxodonta africana', age: 15},
+    {id: 4, name: 'Giraffe', species: 'Giraffa camelopardalis', age: 20},
+    {id: 5, name: 'Zebra', species: 'Equus zebra', age: 8},
+    {id: 6, name: 'Hippopotamus', species: 'Hippopotamus amphibius', age: 25},
+    {id: 7, name: 'Crocodile', species: 'Crocodylus niloticus', age: 30},
+    {id: 8, name: 'Flamingo', species: 'Phoenicopterus', age: 5},
+    {id: 9, name: 'Penguin', species: 'Aptenodytes forsteri', age: 3},
+    {id: 10, name: 'Kangaroo', species: 'Macropus', age: 7},
+    {id: 11, name: 'Koala', species: 'Phascolarctos cinereus', age: 4},
+    {id: 12, name: 'Kookaburra', species: 'Dacelo novaeguineae', age: 2},
+    {id: 13, name: 'Platypus', species: 'Ornithorhynchus anatinus', age: 6},
+    {id: 14, name: 'Wallaby', species: 'Macropus', age: 5},
+    {id: 15, name: 'Dingo', species: 'Canis lupus dingo', age: 8}
 ];
 
 fs.writeFileSync('./data/animals.json', JSON.stringify(animalZoo));
@@ -41,7 +42,7 @@ app.get('/', (req, res) => {
     data.forEach((li) => {
         let liHtml = listItem;
         liHtml = liHtml
-        .replaceAll('{{ID}}', li.id)
+        .replace('{{ID}}', li.id)
         .replace('{{NAME}}', li.name)
         .replace('{{SPECIES}}', li.species)
         .replace('{{AGE}}', li.age);
@@ -62,13 +63,13 @@ app.get('/create', (req, res) => {
 });
 
 app.post('/store', (req, res) => {
-    const name = req.name;
-    const species = req.species;
-    const age = req.age;
-    // const id = uuidv4();
+    const id = req.body.id
+    const name = req.body.name;
+    const species = req.body.species;
+    const age = req.body.age;
     let data = fs.readFileSync('./data/animals.json', 'utf-8');
     data = JSON.parse(data);
-    data.push({ name, species, age });
+    data.push({ id, name, species, age });
     data = JSON.stringify(data);
     fs.writeFileSync('./data/animals.json', data);
     res.redirect(302, 'http://animals');
@@ -81,18 +82,45 @@ app.get('/edit/:id', (req, res) => {
     const animal = data.find(a => a.id === req.params.id);
     let html = fs.readFileSync('./data/edit.html', 'utf-8');
     // const nav = fs.readFileSync('./data/nav.html', 'utf-8');
-    html = html.replace('{{NAME}}', animal.name).replace('{{SPECIES}}', animal.species).replace('{{AGE}}', animal.age);
+    html = html.replace('{{ID}}', animal.id).replace('{{NAME}}', animal.name).replace('{{SPECIES}}', animal.species).replace('{{AGE}}', animal.age);
     res.send(html);
 });
 
 app.post('/update/:id', (req, res) => {
-
+    const id = req.body.id;
     const name = req.body.name;
-    const species = req.species;
-    const age = req.age;
+    const species = req.body.species;
+    const age = req.body.age;
     let data = fs.readFileSync('./data/animals.json', 'utf-8');
     data = JSON.parse(data);
     data = data.map(a => a.id === req.params.id ? {...a, name, species, age} : a);
+    data = JSON.stringify(data);
+    fs.writeFileSync('./data/animals.json', data);
+    res.redirect(302, 'http://animals');
+});
+
+app.get('/delete/:id', (req, res) => {
+
+    let data = fs.readFileSync('./data/animals.json', 'utf-8');
+    data = JSON.parse(data);
+    const animal = data.find(a => a.id === req.params.id);
+
+    if (!animal) {
+        let html = fs.readFileSync('./data/404.html', 'utf-8');
+        res.status(404).send(html);
+    } else {
+        let html = fs.readFileSync('./data/delete.html', 'utf-8');
+    // const nav = fs.readFileSync('./data/nav.html', 'utf-8');
+    html = html.replace('{{ID}}', animal.id).replace('{{NAME}}', animal.name).replace('{{SPECIES}}', animal.species).replace('{{AGE}}', animal.age);
+    res.send(html);
+    }
+});
+
+app.post('/destroy/:id', (req, res) => {
+    
+    let data = fs.readFileSync('./data/animals.json', 'utf-8');
+    data = JSON.parse(data);
+    data = data.filter(a => a.id !== req.params.id);
     data = JSON.stringify(data);
     fs.writeFileSync('./data/animals.json', data);
     res.redirect(302, 'http://animals');
