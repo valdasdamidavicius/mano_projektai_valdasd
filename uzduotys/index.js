76329b31aa81d495bed519bf2247983f64801375
valdasdamidavicius <valdukasirko@gmail.com>
2024-06-04 18:40:19 +0300

    uzduotys

---
+++ uzduotys/index.js
@@ -0,0 +1,28 @@
+console.log('labas');
+
+// 1. Sukurkite ciklą kuris atspausdintų 10 kartų žodį “labas”.
+
+let A = 'Labas';
+
+for (let i = 0; i < 10; i++) {
+    console.log(A);
+}
+
+// 2. Sukurkite ciklą kuris atspausdintų skaičius nuo 0 iki 9.
+
+for (let i = 0; i < 9; i++) {
+    console.log(i);
+}
+
+// 3. Sukurkite masyvą su dešimt augalų pavadinimų.
+
+let plants = ['bukas', 'tuopa', 'klevas', 'maumedis', 'uosis', 'berzas', 'slyva', 'obelis', 'egle', 'pusis'];
+
+// 4. Atspausdinkite kiekvieną 3čio uždavinio augalą atskiroje eilutėje.
+
+for (let i = 0; i < plants.length; i++) {
+    console.log(plants[i]);
+}
+
+// 5. Atspausdinkite 3čio uždavinio kiekvieną augalą pradedant nuo paskutinio, ir baigiant pirmuoju. (atvirkščias ciklas).
+
