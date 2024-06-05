c5e0ded7cf10c6841f434a929cea1b4712469c40
valdasdamidavicius <valdukasirko@gmail.com>
2024-06-05 09:31:22 +0300

    uzduotys

--- uzduotys/index.js
+++ uzduotys/index.js
@@ -26,3 +26,63 @@ for (let i = 0; i < plants.length; i++) {
 
 // 5. Atspausdinkite 3čio uždavinio kiekvieną augalą pradedant nuo paskutinio, ir baigiant pirmuoju. (atvirkščias ciklas).
 
+
+for (let i = plants.length - 1; i >= 0; i--) {
+    console.log(plants[i]);
+}
+
+
+// 6. Atspausdinkite kas antrą skaičių nuo 10 iki 50 (porinius);
+
+for (let i = 10; i <= 50; i += 2) {
+    console.log(i);
+}
+
+let rezultatas = [];
+
+for (let i = 10; i <= 50; i += 2) {
+    rezultatas.push(i);
+}
+
+console.log(rezultatas);
+
+
+// 7. Atspausdinkite kas antrą skaičių nuo 10 iki 50. (pornius) Jei skaičius dalinasi iš 10 be liekanos jo nespausdinkite. ( naudokite continue.)
+// (atspausdinti visus porinius skaičius, išskyrus tuos kurie dalinasi iš 10 be liekanos)
+
+for (let i = 10; i <= 50; i += 2) {
+    if (i % 10 === 0) {
+      continue;
+    }
+    console.log(i);
+}
+  
+
+// 8. Sukurkite ciklą kuris suktųsi nuo 0 iki 20. Suskaičiuokite, kiek kartų kintamasis i turėjo porinę reikšmę;
+
+let poriniuSkaitiklis = 0;
+
+for (let i = 0; i <= 20; i++) {
+  if (i % 2 === 0) {
+    poriniuSkaitiklis++;
+  }
+}
+
+console.log("Kintamasis i turėjo porinę reikšmę " + poriniuSkaitiklis + " kartų.");
+
+// 9. Suskaičiuokite kiek 3čio uždavinio masyve yra žodžių trumpesnių nei 5 simboliai, ir kiek ilgesnių nei 7 simboliai. Skaičiuojam abu atskirai.
+
+let trumpesniNei5 = 0;
+let ilgesniNei7 = 0;
+
+for (let i = 0; i < plants.length; i++) {
+  if (plants[i].length < 5) {
+    trumpesniNei5++;
+  }
+  if (plants[i].length > 7) {
+    ilgesniNei7++;
+  }
+}
+
+console.log("Žodžių trumpesnių nei 5 simboliai: " + trumpesniNei5);
+console.log("Žodžių ilgesnių nei 7 simboliai: " + ilgesniNei7);
