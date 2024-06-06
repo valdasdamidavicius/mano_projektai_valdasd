3b30376e198130ce54b9058d2377253ac9d9c028
valdasdamidavicius <valdukasirko@gmail.com>
2024-06-06 10:53:27 +0300

    uzduotys2

---
+++ uzduotys2/index.js
@@ -0,0 +1,39 @@
+console.log('Sveiki sveiki');
+
+// 1. Sukurkite funkciją kuri priimtų du kintamuosius, juos sudaugintų ir atspausdintų sandaugą į konsolę.
+
+
+// 2. Sukurkite funkciją kuri priimtų vieną kintamąjį, vardą. Ir konsolėje išspausdintų “labas “
+// ir tą paduotą vardą.
+
+
+// 3. Sukurkite funkciją  kuri priimtų vieną kintamąjį, tekstą. Ir konsolėje išspausdintų kiek simbolių yra tame tekste.
+
+
+// 4. Sukurkite funkciją kuri priimtų 2 kintamuosius tekstus, vardą, pavardę ir atspausdintų inicialus.
+// T.y vardo ir pavardės pirmąsias raides DIDŽIOSIOMIS raidėmis.
+
+
+// 5. Sukurkite HTML div’ą su id “numberPlace”. Parašykite funkciją kuri priimtų kintamąjį, skaičių ir jį atspausdintų tame HTML elemente.
+
+
+// 6. Sukurkite Funkciją kuri priima masyva, prasuka ciklą ir atspausdina (consolėje) 
+// kiekvieną elementą vienoje eilutėje, atskirtus kableliais, po paskutinio elemento kabliataškis.
+
+
+// 7. Parašykite funkciją kuri priimtų du kintamuosius “nuo” ir “iki”. Funkcija turi sugeneruoti random skaičių tame intervale ir jį GRAŽINTI.
+
+
+// 8. Sukurkite Funkciją kuri sugeneruotų random sveikųjų skaičių masyvą ir jį gražintų.
+// Funkcija priima tris int tipo kintamuosius, min, max ir length reikšmėms nustatyti.
+
+
+// 9. Sukurkite Funkciją kuri panaudotų 8toje užduotyje sugeneruotą masyvą (priimtų kaip kintamąjį), susumuotų ir gražintų reikšmę.
+
+
+// 10. Sukurkite Funkciją kuri priimtų 8toje užduotyje sugeneruotą masyvą ir gražintų jos skaičių vidurkį (double).
+
+
+// 11. Sukurkite HTML div’ą su id “sequence”. Parašykite funkciją kuri sugeneruotų 10 p tagų 
+// su skaičiais juose nuo 1 iki 10 ir atiduotų į tą HTML elementą. 
+// Rezultate HTML’e turi matytis 10 p tagų su skaičiais. Šie TURI BŪTI SUGENERUOTI JAVASCRIPTU.
